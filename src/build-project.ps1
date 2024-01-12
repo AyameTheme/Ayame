352b90a98edc869aa68de7fde65788d0d5f6bfe6
Nurdoidz <nurdoidz@live.com>
2024-01-11 17:45:17 -0700

    Add Stylus

--- src/build-project.ps1
+++ src/build-project.ps1
@@ -5,47 +5,35 @@ $Ayame = Get-Content '.\ayame-colors.json' -Raw | ConvertFrom-Json
 
 # --( Path Variables )----------------------------------------------------------
 
-$CSSVariablesPath = '..\out\ayame-variables.css'
+$AyameVariablesPath = 'ayame-variables.styl'
 $IconsPath = '..\out\icon'
 $ReadmePath = '..\README.md'
 $ReadmeTemplatePath = '.\readme-template.md'
 
-# --( out/ayame-variables.css )-------------------------------------------------
+# --( ayame-variables.styl )----------------------------------------------------
 
-$AliasesCount = 0
-foreach ($Color in $Ayame.colors) {
-    $AliasesCount += $Color.aliases.Count
-}
+$Prefix = '--ayame-'
 
-$CSSAliases = [string[]]::new($AliasesCount)
-$CSSVariables = [string[]]::new($Ayame.colors.Count)
-$AliasIdx = 0
-
-for ($ColorIdx = 0; $ColorIdx -lt $Ayame.colors.Count; $ColorIdx++) {
-    $ThisColor = $Ayame.colors[$ColorIdx]
-    $CSSVariables[$ColorIdx] = "  --ayame-$($ThisColor.name): $($ThisColor.hex);"
-    
-    if ($ThisColor.uses.Count -gt 0) {
-        $CSSVariables[$ColorIdx] += " /* $($ThisColor.uses -Join ", ") */"
+Set-Content -Path $AyameVariablesPath -Value @"
+$(($Ayame.colors | ForEach-Object { "a-$($_.name) = $($Prefix)$($_.name)" }) -Join "`n")
+:root
+$(($Ayame.colors | ForEach-Object {
+    $Comment = ''
+    if ($_.uses -gt 0) {
+        $Comment = "/* $($_.uses -Join ', ') */"
     }
-
-    for ($ThisAliasIdx = 0; $ThisAliasIdx -lt $ThisColor.aliases.Count; $ThisAliasIdx++) {
-        $ThisAlias = $ThisColor.aliases[$ThisAliasIdx]
-        $CSSAliases[$AliasIdx] = "  --ayame-$($ThisAlias): var(--ayame-$($ThisColor.name));"
-
-        if ($ThisColor.uses.Count -gt 0) {
-            $CSSAliases[$AliasIdx] += " /* $($ThisColor.uses -Join ", ") */"
+    "    $($Prefix)$($_.name) $($_.hex);$Comment"
+}) -Join "`n")
+$(($Ayame.colors | Where-Object { $_.aliases } | ForEach-Object {
+    $ThisColor = $_
+    ($_.aliases | ForEach-Object {
+        $Comment = ''
+        if ($ThisColor.uses -gt 0) {
+            $Comment = " /* $($ThisColor.uses -Join ', ') */"
         }
-
-        $AliasIdx++
-    }
-}
-
-Set-Content -Path $CSSVariablesPath -Value @"
-:root {
-$($CSSVariables -Join "`n")
-$($CSSAliases -Join "`n")
-}
+        "    $($Prefix)$_ var(a-$($ThisColor.name));$Comment"
+    }) -Join "`n"
+}) -Join "`n")
 "@
 
 # --( out/icon/*.svg ) ---------------------------------------------------------
@@ -54,7 +42,7 @@ if (Test-Path $IconsPath) {
     Get-ChildItem $IconsPath | ForEach-Object { Remove-Item $_ -Recurse }
 }
 else {
-    New-Item -ItemType Directory -Path $IconsPath -Force
+    New-Item -ItemType Directory -Path $IconsPath -Force | Out-Null
 }
 
 foreach ($Color in $Ayame.colors) {
@@ -64,29 +52,27 @@ foreach ($Color in $Ayame.colors) {
   <circle cx="8" cy="8" r="8" fill="$($Color.hex)" />
 </svg>
 "@
-    New-Item -Path $SVGPath -Value $SVGContent
+    New-Item -Path $SVGPath -Value $SVGContent | Out-Null
 }
 
 # --( README.md ) --------------------------------------------------------------
 
-$AyameTableRows = [string[]]::new($Ayame.colors.Count)
 $IconURL = 'https://raw.githubusercontent.com/Nurdoidz/Ayame/master/out/icon/'
 
-for ($ColorIdx = 0; $ColorIdx -lt $Ayame.colors.Count; $ColorIdx++) {
-    $ThisColor = $Ayame.colors[$ColorIdx]
-    $AyameTableRows[$ColorIdx] = "| ![]($($IconURL)$($ThisColor.name).svg) ``$($ThisColor.hex)`` |"
-    
-    $TheseNames = [string[]]::new($ThisColor.aliases.Count + 1)
-    $TheseNames[0] = "``$($ThisColor.name)``"
-    for ($i = 1; $i -le $ThisColor.aliases.Count; $i++) {
-        $TheseNames[$i] = "``$($ThisColor.aliases[$i - 1])``"
-    }
+# Don’t waste three hours of your life like me and just accept the assignment
+$Backtick = "``"
 
-    $AyameTableRows[$ColorIdx] += " $($TheseNames -Join ", ") |"
-    $AyameTableRows[$ColorIdx] += " $($ThisColor.uses -Join ", ") |"
-}
-
-$AyamePaletteTable = ($AyameTableRows -Join "`n")
+$AyamePaletteTable = ($Ayame.colors | ForEach-Object {
+    @(
+        "| ![]($($IconURL)$($_.name).svg) ``$($_.hex)`` |",
+        " ``$($_.name)``",
+        "$($_.aliases.Count -gt 0 ? ', ' : '')",
+        "$(($_.aliases | ForEach-Object {
+            "$Backtick$_$Backtick"
+        }) -Join ', ') |",
+        " $($_.uses -Join ', ') |"
+    ) -Join ''
+}) -Join "`n"
 
 (Get-Content $ReadmeTemplatePath).Replace('@-ayame-palette-table', $AyamePaletteTable) | Set-Content $ReadmePath
 
